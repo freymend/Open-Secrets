0ac03a60eb0a2faed41f6571d2f75c1cbb5e2a9d
freymond <37636251+freymend@users.noreply.github.com>
2023-12-04 13:03:24 -0800

    create login/signup views

--- app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
@@ -1,11 +1,15 @@
 package edu.uw.ischool.opensecrets
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.uw.ischool.opensecrets.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
