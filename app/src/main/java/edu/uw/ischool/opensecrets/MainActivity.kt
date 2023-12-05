b6a88c8054327fa499cdd20ff30d4698bf25a039
Duy Nguyen <duyhoangnguyen2000@gmail.com>
2023-12-05 12:33:37 -0800

    updated MainActivity for homepage

--- app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
@@ -1,11 +1,9 @@
 package edu.uw.ischool.opensecrets
 
 import android.content.Intent
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import edu.uw.ischool.opensecrets.auth.LoginActivity
 import java.io.File
 import android.util.Log
@@ -19,17 +17,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.searchButton.setOnClickListener {
-            startActivity(Intent(this, SearchActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
-        }
-        binding.addButton.setOnClickListener {
-            startActivity(Intent(this, EntryTextActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
-        }
-        binding.optionButton.setOnClickListener {
-            startActivity(Intent(this, OptionActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
-        }
         Log.i("MainActivity", filesDir.toString())
 
         val journal = File(filesDir, "journal.json")
@@ -37,6 +24,18 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
         } else {
 //            TODO: put home screen here
+
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            binding.searchButton.setOnClickListener {
+                startActivity(Intent(this, SearchActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            }
+            binding.addButton.setOnClickListener {
+                startActivity(Intent(this, EntryTextActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            }
+            binding.optionButton.setOnClickListener {
+                startActivity(Intent(this, OptionActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+            }
         }
     }
 }
