62b2409a5c9ef3fd4fc7d6f2ab9dfc6c0ad54d94
Zachary <37636251+freymend@users.noreply.github.com>
2023-12-04 18:50:44 -0800

    create login/signup views (#24)
    * create login/signup views
    
    * handle wrong credentials
    
    * temp logic for on first time start up

--- app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
@@ -1,11 +1,22 @@
 package edu.uw.ischool.opensecrets
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import edu.uw.ischool.opensecrets.auth.LoginActivity
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.i("MainActivity", filesDir.toString())
+
+        val journal = File(filesDir, "journal.json")
+        if (!journal.exists()) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
+//            TODO: put home screen here
+        }
     }
 }
---
+++ app/src/main/java/edu/uw/ischool/opensecrets/auth/LoginActivity.kt
@@ -0,0 +1,78 @@
+package edu.uw.ischool.opensecrets.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import edu.uw.ischool.opensecrets.MainActivity
+import edu.uw.ischool.opensecrets.R
+import org.json.JSONObject
+import java.io.BufferedOutputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val login = findViewById<Button>(R.id.login)
+
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+
+        username.addTextChangedListener {
+            login.isEnabled = username.text.isNotEmpty() && password.text.isNotEmpty()
+        }
+        password.addTextChangedListener {
+            login.isEnabled = username.text.isNotEmpty() && password.text.isNotEmpty()
+        }
+
+        findViewById<Button>(R.id.sign_up).setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+        login.setOnClickListener {
+            Thread {
+                val response =
+                    with(URL("https://not-open-secrets.fly.dev/login").openConnection() as HttpURLConnection) {
+                        requestMethod = "POST"
+
+                        connectTimeout = 5000
+
+                        setRequestProperty("Content-Type", "application/json")
+                        setRequestProperty("Accept", "application/json")
+
+                        doOutput = true
+                        setChunkedStreamingMode(0)
+                        BufferedOutputStream(outputStream).use {
+                            it.write(
+                                """{
+                                    "username": "${username.text}",
+                                    "password": "${password.text}"
+                                }""".trimIndent().toByteArray()
+                            )
+                            it.flush()
+                        }
+
+                        BufferedReader(InputStreamReader(inputStream)).use {
+                            JSONObject(it.readText())
+                        }
+                    }
+
+                runOnUiThread {
+                    if (response.getBoolean("authenticated")) {
+//                        TODO: put home screen here
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(this, getString(R.string.login_failed), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }.start()
+        }
+    }
+}
