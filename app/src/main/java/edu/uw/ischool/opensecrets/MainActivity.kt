7c6e0c15b4f69677e03995d1e532a0e21aa084d1
Gitrogatog <rpokexy@gmail.com>
2023-12-06 15:40:46 -0800

    Completed core ContactMessager behavior as ordinary Kotlin class

--- app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/opensecrets/MainActivity.kt
@@ -2,10 +2,17 @@ package edu.uw.ischool.opensecrets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val messager : ContactMessager = ContactMessager()
+        messager.getContactList(this)
+        for(contact : Contact in messager.contactList){
+            Log.i("MAIN ACTIVITY", "${contact.name}: ${contact.phoneNumber}")
+        }
+//        Log.i("MAIN ACTIVITY", "${messager.contactList}")
     }
 }
