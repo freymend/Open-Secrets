c564de059da461cc59469cce9c142b0ecaef53f1
Duy Nguyen <duyhoangnguyen2000@gmail.com>
2023-12-14 00:05:35 -0800

    Fixing alarm manager off-timing.

--- app/src/main/java/edu/uw/ischool/opensecrets/model/TimedMessageQueue.kt
+++ app/src/main/java/edu/uw/ischool/opensecrets/model/TimedMessageQueue.kt
@@ -4,39 +4,36 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Context.ALARM_SERVICE
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
-import android.os.SystemClock
 import android.util.Log
-import java.lang.System.currentTimeMillis
-import java.time.Instant
 
 const val ALARM_ACTION = "OpenSecretMessageAlarm"
 class TimedMessageQueue(val context : Context, val alarmManager: AlarmManager, val sendEntryLambda : (Entry) -> Unit) {
-    val queuedEntries : ArrayList<Entry> = ArrayList<Entry>()
+    val queuedEntries : MutableMap<Int, Entry> = mutableMapOf<Int, Entry>()
 //    val queuedPendings : ArrayList<PendingIntent> = ArrayList<PendingIntent>()
     var receiver : BroadcastReceiver? = null
     var requestCode : Int = 0
 
     fun queueSendEntry(entry : Entry, minutesToSend : Long){
-        requestCode += 1
+        requestCode = System.currentTimeMillis().toInt()
         if(receiver == null){
             receiver = object : BroadcastReceiver() {
                 override fun onReceive(context: Context?, intent: Intent?) {
-                    sendNextEntryInQueue()
+                    Log.i("Time", "Request code ${intent?.extras?.getInt("requestCode")}")
+                    sendNextEntryInQueue(intent?.extras?.getInt("requestCode"))
                 }
             }
 
             val filter = IntentFilter(ALARM_ACTION)
             context.registerReceiver(receiver, filter)
         }
-        val intent = Intent(ALARM_ACTION)
+        val intent = Intent(ALARM_ACTION).putExtra("requestCode", requestCode)
 
         val pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, PendingIntent.FLAG_IMMUTABLE)
 //        queuedPendings.add(pendingIntent)
-        queuedEntries.add(entry)
+        queuedEntries[requestCode] = entry
         if(Build.VERSION.SDK_INT < 31 || alarmManager.canScheduleExactAlarms()){
             Log.i("TimedMessageQueue", "sending entry ${entry.title} in $minutesToSend minutes")
             alarmManager.setExact(AlarmManager.RTC_WAKEUP,
@@ -47,17 +44,19 @@ class TimedMessageQueue(val context : Context, val alarmManager: AlarmManager, v
             Log.i("TimedMessageQueue", "Message Queue failed you, sorry")
         }
     }
-    fun sendNextEntryInQueue(){
+    fun sendNextEntryInQueue(requestCode: Int?) {
 //        if(queuedPendings.size > 0){
 //            queuedPendings.removeFirstOrNull()
 //        }
-        if(queuedEntries.size > 0){
-            val entryToSend : Entry = queuedEntries.removeFirst()
-            sendEntryLambda(entryToSend)
+        if(queuedEntries.isNotEmpty()){
+            val entryToSend : Entry? = queuedEntries.remove(requestCode)
+            if (entryToSend != null) {
+                sendEntryLambda(entryToSend)
+            }
         }
     }
     fun sendAllEntries(){
-        for(entry : Entry in queuedEntries){
+        for((key, entry : Entry) in queuedEntries){
             sendEntryLambda(entry)
         }
 //        for(pendingIntent : PendingIntent in queuedPendings){
